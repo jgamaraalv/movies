6b35bb5cc5739f58480df1beefd96524ec02b4f9
João Vequiato <guilive13@gmail.com>
2025-12-16 22:37:45 -0300

    adds a basic logger

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/jgamaraalv/movies.git/logger"
 )
 
 func main() {
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8080"
+	logInstance.Info("Server starting on " + addr)
+
 	if err := http.ListenAndServe(addr, nil); err != nil {
+		logInstance.Error("Server failed to start", err)
 		log.Fatalf("Server failed: %v", err)
 	}
+}
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie-service.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	return logInstance
 }
