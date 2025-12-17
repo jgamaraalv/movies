bb31a683575a60ea44b816b9598e96b7ffdb244a
João Vequiato <guilive13@gmail.com>
2025-12-17 10:39:38 -0300

    adds error message modal

--- public/app.js
+++ public/app.js
@@ -4,6 +4,18 @@ import Router from "./services/Router.js";
 
 window.app = {
   Router,
+  showError: (
+    message = "There was an error loading the page",
+    goToHome = true
+  ) => {
+    document.querySelector("#alert-modal").showModal();
+    document.querySelector("#alert-modal p").textContents = message;
+    if (goToHome) app.Router.go("/");
+    return;
+  },
+  closeError: () => {
+    document.getElementById("alert-modal").close();
+  },
   search: (event) => {
     event.preventDefault();
     const keywords = document.querySelector("input[type=search]").value;
