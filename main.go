5a6589379f78a9318e52a51df7b6e2c99991bb1a
João Vequiato <guilive13@gmail.com>
2025-12-16 22:06:33 -0300

    starts a basic go server

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = ":8080"
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
