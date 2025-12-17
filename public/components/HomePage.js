3653d43ed25a99832702d54f23b44916685ab04c
João Vequiato <guilive13@gmail.com>
2025-12-17 03:45:21 -0300

    adds client's movie-item component and adds API fetch to the home-page component

--- public/components/HomePage.js
+++ public/components/HomePage.js
@@ -1,8 +1,31 @@
+import API from "../services/API.js";
+import MovieItemComponent from "./MovieItem.js";
+
 export default class HomePage extends HTMLElement {
+   async render() {
+        const topMovies = await API.getTopMovies();
+        renderMoviesInList(topMovies, this.querySelector("#top-10 ul"));
+
+        const randomMovies = await API.getRandomMovies();
+        renderMoviesInList(randomMovies, this.querySelector("#random ul"));
+
+        function renderMoviesInList(movies, ul) {
+            const fragment = document.createDocumentFragment();
+            movies.forEach(movie => {
+                const li = document.createElement("li");
+                li.appendChild(new MovieItemComponent(movie));
+                fragment.appendChild(li);
+            });
+            ul.appendChild(fragment);
+        }
+    }
+
+
     connectedCallback() {
         const template = document.getElementById("template-home");
         const content = template.content.cloneNode(true);
         this.appendChild(content);  
+        this.render();
     }
 }
 
